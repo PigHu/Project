719c10d6a9b7dd2511e22dcf815c1f0d9db503a3
Joe <HaikunHu@163.com>
2021-01-29 13:42:01 +0800

    主页功能板块开发完成

---
+++ js/index.js
@@ -0,0 +1,47 @@
+$(function() {
+    var layer = layui.layer;
+    getUserInfo();
+    // 点击退出按钮 退出系统
+    $('#btnGoOut').on('click', function() {
+        layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+            // 关闭弹出层返回的页面
+            layer.close(index);
+        });
+    });
+});
+//获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+
+        success: function(res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败');
+            }
+            // 调用renderAvatar() 渲染用户头像
+            renderAvatar(res.data);
+        },
+    });
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+    // 获取用户名称
+    var name = user.nickname || user.username;
+    // 渲染用户名称
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 按需渲染用户头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
+}
